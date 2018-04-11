982a547e3ae0bd2923ec6a6a83b44177ba10b7ad
xiaoweihua <xiaoweihua@sensetime.com>
2018-04-11 11:21:23 +0800

    更新代码

--- rightSide/ViewController.m
+++ rightSide/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 #import "RightViewController.h"
 #import "CenterViewController.h"
-#import "UIView+MJ.h"
 
 #define SideWidth 150
 
@@ -31,13 +30,17 @@
     [self addChildViewController:center];
     
     //右边
-    RightViewController *right = [[RightViewController alloc]init];
-    right.view.width = SideWidth;
-    right.view.x = self.view.frame.size.width;
-    right.view.y = 20;
-    [self.view addSubview:right.view];
-    [self addChildViewController:right];
-    self.right = right;
+    self.right = [[RightViewController alloc]init];
+
+    CGRect frameTemp = self.right.view.frame;
+    frameTemp.size.width = SideWidth;
+    frameTemp.origin.x = self.view.frame.size.width;
+    frameTemp.origin.y = 20;
+
+    self.right.view.frame = frameTemp;
+
+    [self.view addSubview:self.right.view];
+    [self addChildViewController:self.right];
     
     //手势 拖拽
     [center.view addGestureRecognizer:[[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panChange:)]];
@@ -47,7 +50,7 @@
 
 - (void)tapChange:(UITapGestureRecognizer *)tap
 {
-    if (self.right.view.origin.x == (self.view.frame.size.width-SideWidth)) {
+    if (self.right.view.frame.origin.x == (self.view.frame.size.width-SideWidth)) {
         [UIView animateWithDuration:0.5 animations:^{
             self.right.view.transform = CGAffineTransformIdentity;
         }];
@@ -59,7 +62,7 @@
     CGPoint point = [pan translationInView:pan.view];
 
     if (pan.state == UIGestureRecognizerStateCancelled||pan.state == UIGestureRecognizerStateEnded) {
-        if (self.right.view.x<=(self.view.frame.size.width- SideWidth*0.5)) {// 往左边至少走动了75
+        if (self.right.view.frame.origin.x<=(self.view.frame.size.width- SideWidth*0.5)) {// 往左边至少走动了75
             [UIView animateWithDuration:0.5 animations:^{
                 self.right.view.transform = CGAffineTransformMakeTranslation(- SideWidth, 0);
             }];
@@ -74,10 +77,10 @@
         self.right.view.transform = CGAffineTransformTranslate(self.right.view.transform, point.x, 0);
         [pan setTranslation:CGPointZero inView:self.right.view];
         
-        if (self.right.view.x<= (self.view.size.width- SideWidth)) {
+        if (self.right.view.frame.origin.x<= (self.view.frame.size.width- SideWidth)) {
             self.right.view.transform = CGAffineTransformMakeTranslation(- SideWidth, 0);
             [[NSNotificationCenter defaultCenter]postNotificationName:@"buttonNameChange" object:@"隐藏"];
-        }else if (self.right.view.x>=self.view.frame.size.width){
+        }else if (self.right.view.frame.origin.x>=self.view.frame.size.width){
             self.right.view.transform = CGAffineTransformIdentity;
             [[NSNotificationCenter defaultCenter]postNotificationName:@"buttonNameChange" object:@"显示"];
         }
@@ -87,12 +90,12 @@
 - (void)centerViewController:(CenterViewController *)centerViewController didButton:(UIButton *)btn
 {
 
-    if (self.right.view.origin.x == (self.view.frame.size.width-SideWidth)) {
+    if (self.right.view.frame.origin.x == (self.view.frame.size.width-SideWidth)) {
         [UIView animateWithDuration:0.5 animations:^{
             self.right.view.transform = CGAffineTransformIdentity;
             [[NSNotificationCenter defaultCenter]postNotificationName:@"buttonNameChange" object:@"显示"];
         }];
-    }else if(self.right.view.superview.origin.x == 0){
+    }else if(self.right.view.superview.frame.origin.x == 0){
         [UIView animateWithDuration:0.5 animations:^{
             self.right.view.transform = CGAffineTransformMakeTranslation(- SideWidth, 0);
             [[NSNotificationCenter defaultCenter]postNotificationName:@"buttonNameChange" object:@"隐藏"];
